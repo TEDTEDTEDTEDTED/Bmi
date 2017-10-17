d25fec739fc8f781b1f234d6ca95ed59c49e20c2
TEDTEDTEDTEDTED <lf2netted@gmail.com>
2017-10-17 16:20:44 +0800

    Initial commit

---
+++ app/src/main/java/com/tedtedtedtedted/bmi/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tedtedtedtedted.bmi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.ypnors.bmi.R.layout.activity_main);
+    }
+}
