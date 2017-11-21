d70414c3d9f1fa6c498537799073fc52be6a43a6
TEDTEDTEDTEDTED <lf2netted@gmail.com>
2017-11-21 12:06:29 +0800

    GG

--- app/src/main/java/com/tedtedtedtedted/bmi/MainActivity.java
+++ app/src/main/java/com/tedtedtedtedted/bmi/MainActivity.java
@@ -1,4 +1,8 @@
 package com.tedtedtedtedted.bmi;
+
+import android.content.Intent;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,11 +14,15 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText edWeight;
+    private EditText edHeight;
+    private Button bhelp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button bhelp =(Button) findViewById(R.id.b_help);
+        findviews();
         bhelp.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 new AlertDialog.Builder(MainActivity.this)
@@ -29,14 +37,24 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    public void findviews(){
+        bhelp = (Button) findViewById(R.id.b_help);
+        edWeight = (EditText) findViewById(R.id.ed_weight);
+        edHeight = (EditText) findViewById(R.id.ed_height);
+    }
     public void bmi(View view){
-        EditText edWeight =(EditText) findViewById(R.id.ed_weight);
-        EditText edHeight =(EditText) findViewById(R.id.ed_height);
+
         String w = edWeight.getText().toString();
         String h = edHeight.getText().toString();
         float weight = Float.parseFloat(w);
         float height = Float.parseFloat(w);
         float bmi = weight/(height*height);
+        Bundle bag = new Bundle();
+        bag.putFloat(getString(R.string.bmi_extra),bmi);
+        bag.putString("TEST_EXTRA","bagtesting");
+        Intent intent = new Intent(this,ResultActivity.class);
+        intent.putExtra("BMI_EXTRA",bmi);
+        startActivity(intent);
         if(height>3){
             new AlertDialog.Builder(this)
                     .setMessage(bmi+"身高單位應為公尺")
@@ -48,5 +66,35 @@ public class MainActivity extends AppCompatActivity {
                     .setMessage(bmi+"請多吃點")
                     .show();
         }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
     }
 }
---
+++ app/src/main/java/com/tedtedtedtedted/bmi/ResultActivity.java
@@ -0,0 +1,21 @@
+package com.tedtedtedtedted.bmi;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        Intent intent=getIntent();
+        float bmi = intent.getFloatExtra("BMI_EXTRA",0);
+        Bundle bag =intent.getExtras();
+        String test=bag.getString("TEST_EXTRA",null);
+        TextView result =(TextView) findViewById(R.id.result);
+        result.setText("您的BMI值為:"+bmi+test);
+    }
+}
